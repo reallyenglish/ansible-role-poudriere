9c0686a2df8d8fcc067d5ccbee1f3c5169d761d8
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-01-15 18:08:34 +0700

    Add Integration test (#4)
    * QA
    
    * add .keepme
    
    * add Jenkinsfile
    
    * update meta
    
    * remove svn ports tree from the test
    
    to make the test faster, it takes too long
    
    * add integration test

---
+++ tests/integration/build_packages/spec/spec_helper.rb
@@ -0,0 +1,25 @@
+require 'infrataster/rspec'
+
+ENV['VAGRANT_CWD'] = File.dirname(__FILE__)
+ENV['LANG'] = 'C'
+
+Infrataster::Server.define(
+  :server1,
+  '192.168.21.100',
+  vagrant: true,
+)
+
+def json_body_as_hash
+    json_str_to_hash(response.body)
+end
+
+def json_str_to_hash(str)
+  JSON.parse(str)
+end
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  config.order = 'default'
+end
