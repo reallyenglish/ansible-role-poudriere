a0fb5b766ff0d53c3b5d474dc9bd444ce4eb7490
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-02-23 12:37:09 +0700

    [bugfix] use freebsd-ports-mini (#10)
    * `method` can be empty
    
    when you have a ports tree on local file system, `-m` should not be set.
    
    * use freebsd-ports-mini to make the test faster
    
    * use freebsd-ports-mini
    
    * connect to the build

--- tests/integration/build_packages/spec/default_spec.rb
+++ tests/integration/build_packages/spec/default_spec.rb
@@ -2,8 +2,8 @@ require "spec_helper"
 
 context "after provision finishes" do
   describe server(:server1) do
-    it "builds ports-mgmt/pkg" do
-      result = current_server.ssh_exec "sudo poudriere bulk -p 10_3_re -j 10_3 -C ports-mgmt/pkg && echo SUCCESSFULL_BIULD"
+    it "builds ports-mgmt/portmaster" do
+      result = current_server.ssh_exec "sudo poudriere bulk -p mini -j 10_3 -C ports-mgmt/portmaster && echo SUCCESSFULL_BIULD"
       expect(result).to match(/SUCCESSFULL_BIULD/)
     end
   end
