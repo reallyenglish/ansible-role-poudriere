7519b181ebdf3d0cca5b920015ef8a7db8a647fc
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-05-27 20:29:23 +0900

    QA

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,9 +1,9 @@
-require 'spec_helper'
-require 'serverspec'
+require "spec_helper"
+require "serverspec"
 
-package = 'poudriere'
-config  = '/usr/local/etc/poudriere.conf'
-basefs = '/usr/local/poudriere'
+package = "poudriere"
+config  = "/usr/local/etc/poudriere.conf"
+basefs = "/usr/local/poudriere"
 jail_hook_files = %w(jail.sh builder.sh)
 default_owner = "root"
 default_group = "wheel"
@@ -43,12 +43,12 @@ jail_hook_files.each do |f|
     it { should be_owned_by default_owner }
     it { should be_grouped_into default_group }
     it { should be_mode 755 }
-    its(:content) { should match(Regexp.escape('#!/bin/sh')) }
+    its(:content) { should match(Regexp.escape("#!/bin/sh")) }
     its(:content) { should match(Regexp.escape('echo "args=$*"')) }
   end
 end
 
-describe command('poudriere ports -l') do
+describe command("poudriere ports -l") do
   quoted = Regexp.escape("#{basefs}/ports/mini")
   its(:exit_status) { should eq 0 }
   its(:stdout) { should match(/^mini\s+git\s+(?:\d+-\d+-\d+\s+\d+:\d+:\d+\s+)?#{quoted}/) }
@@ -57,7 +57,7 @@ end
 
 describe command("cd #{basefs}/ports/mini && git config --get remote.origin.url") do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(Regexp.escape('https://github.com/reallyenglish/freebsd-ports-mini.git')) }
+  its(:stdout) { should match(Regexp.escape("https://github.com/reallyenglish/freebsd-ports-mini.git")) }
   its(:stderr) { should match(/^$/) }
 end
 
@@ -67,7 +67,7 @@ describe command("cd #{basefs}/ports/mini && git branch") do
   its(:stderr) { should match(/^$/) }
 end
 
-describe command('poudriere jail -l') do
+describe command("poudriere jail -l") do
   quoted = Regexp.escape("#{basefs}/jails/10_3")
   its(:exit_status) { should eq 0 }
   its(:stdout) { should match(/^10_3\s+10\.3-RELEASE(?:-p\d+)?\s+amd64\s+http\s+(?:\d+-\d+-\d+\s+\d+:\d+:\d+\s+)?#{quoted}/) }
