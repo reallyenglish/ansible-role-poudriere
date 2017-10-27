b04e7709258de3de20c8d2465bb0d814e625288b
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-10-27 11:36:47 +0900

    QA

--- tests/integration/build_packages/spec/default_spec.rb
+++ tests/integration/build_packages/spec/default_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 context "after provision finishes" do
-  [ :server1, :server2 ].each do |sym|
+  [:server1, :server2].each do |sym|
     describe server(sym) do
       it "builds ports-mgmt/portmaster" do
         result = current_server.ssh_exec "sudo poudriere bulk -p mini -j 10_3 -C ports-mgmt/portmaster && echo SUCCESSFULL_BIULD"
