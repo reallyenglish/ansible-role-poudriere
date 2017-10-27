2aa0fba95ae7b6a90296bd49784fc54cad5bb68c
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-10-27 11:12:31 +0900

    [feature] support FreeBSD 11.1

--- tests/integration/build_packages/spec/default_spec.rb
+++ tests/integration/build_packages/spec/default_spec.rb
@@ -1,10 +1,12 @@
 require "spec_helper"
 
 context "after provision finishes" do
-  describe server(:server1) do
-    it "builds ports-mgmt/portmaster" do
-      result = current_server.ssh_exec "sudo poudriere bulk -p mini -j 10_3 -C ports-mgmt/portmaster && echo SUCCESSFULL_BIULD"
-      expect(result).to match(/SUCCESSFULL_BIULD/)
+  [ :server1, :server2 ].each do |sym|
+    describe server(sym) do
+      it "builds ports-mgmt/portmaster" do
+        result = current_server.ssh_exec "sudo poudriere bulk -p mini -j 10_3 -C ports-mgmt/portmaster && echo SUCCESSFULL_BIULD"
+        expect(result).to match(/SUCCESSFULL_BIULD/)
+      end
     end
   end
 end
